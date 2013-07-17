bdff57b34a27affad2fefe341037e6ba5911504d
Kimani Walters <flowartic@gmail.com>
2013-07-17 09:57:56 -0500

    updated template structure

--- urls.py
+++ urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^/$', TemplateView.as_view(template_name='pressphotos.html'), name='home'),
+    url(r'^/$',
+        TemplateView.as_view(template_name='django_press_gallery/pressphotos.html'),
+        name='home'
+    ),
 )
