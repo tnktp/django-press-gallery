efb185b6c452448b60d79d5fa49bf7a832625f8f
Kimani Walters <flowartic@gmail.com>
2013-07-23 17:20:07 -0500

    Resize images at higher quality

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.utils.encoding import force_unicode
 from django.utils.functional import Promise
+from stdimage import StdImageField
 
 class LazyEncoder(simplejson.JSONEncoder):
     def default(self, o):
@@ -17,3 +18,50 @@ class JSONResponse(HttpResponse):
             mimetype="application/json",
             status=status,
         )
+
+
+class DPGImageField(StdImageField):
+    def _resize_image(self, filename, size):
+        """Resizes the image to specified width, height and force option
+
+        Arguments::
+
+        filename -- full path of image to resize
+        size -- dictionary with
+            - width: int
+            - height: int
+            - force: bool
+                if True, image will be cropped to fit the exact size,
+                if False, it will have the bigger size that fits the specified
+                size, but without cropping, so it could be smaller on width
+                or height
+
+        """
+
+        WIDTH, HEIGHT = 0, 1
+        try:
+            import Image, ImageOps
+        except ImportError:
+            from PIL import Image, ImageOps
+        img = Image.open(filename)
+        if (img.size[WIDTH] > size['width'] or
+            img.size[HEIGHT] > size['height']):
+
+            #If the image is big resize it with the cheapest resize algorithm
+            # factor = 1
+            # while (img.size[0]/factor > 2*size['width'] and
+            #        img.size[1]*2/factor > 2*size['height']):
+            #     factor *=2
+            # if factor > 1:
+            #     img.thumbnail((int(img.size[0]/factor),
+            #                    int(img.size[1]/factor)), Image.NEAREST)
+
+            if size['force']:
+                img = ImageOps.fit(img, (size['width'], size['height']),
+                                   Image.ANTIALIAS)
+            else:
+                img.thumbnail((size['width'], size['height']), Image.ANTIALIAS)
+            try:
+                img.save(filename, optimize=1)
+            except IOError:
+                img.save(filename)
