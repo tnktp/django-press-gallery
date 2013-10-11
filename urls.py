54d0a73d37676275d238a9d1ee3c03da86fc6b2c
Kimani Walters <flowartic@gmail.com>
2013-10-11 08:51:28 -0500

    Renamed am_distribution url names

--- urls.py
+++ urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 from am_distribution.views import MediaSet, Media, DownloadFILE, DownloadFileFormat, Login
  
 urlpatterns = patterns('',
-    url(r'^/$', MediaSet.as_view(), name='home'),
-    url(r'^/(?P<id>\d+)/([\w-]+)$', Media.as_view(), name='media'),
-    url(r'^/download/(?P<id>\d+)$', DownloadFILE.as_view(), name='download'),
-    url(r'^/download_files/(?P<mediaset_id>\d+)/(?P<version>[\w-]+)$', DownloadFileFormat.as_view(), name='download_files'),
+    url(r'^/$', MediaSet.as_view(), name='am_distribution_home'),
+    url(r'^/(?P<id>\d+)/([\w-]+)$', Media.as_view(), name='am_distribution_media'),
+    url(r'^/download/(?P<id>\d+)$', DownloadFILE.as_view(), name='am_distribution_download'),
+    url(r'^/download_files/(?P<mediaset_id>\d+)/(?P<version>[\w-]+)$', DownloadFileFormat.as_view(), name='am_distribution_download_files'),
     url(r'/login', Login.as_view(), name='login')
 )
